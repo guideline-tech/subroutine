e668ea1143f70d0b19a9f5715cb7954ad682594b
Mike Nelson <mike@guideline.com>
2019-03-15 01:05:55 -0400

    bump version

--- lib/subroutine/version.rb
+++ lib/subroutine/version.rb
@@ -1,10 +1,10 @@
-module Subroutine
+# frozen_string_literal: true
 
+module Subroutine
   MAJOR = 0
-  MINOR = 6
-  PATCH = 3
+  MINOR = 7
+  PATCH = 0
   PRE   = nil
 
   VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
-
 end
