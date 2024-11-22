a5f6ef6e00c68b01ca53b7616da2b1f5db81e772
Mike Nelson <mike@mnelson.io>
2024-11-22 15:46:34 -0500

    add test

--- lib/subroutine.rb
+++ lib/subroutine.rb
@@ -11,9 +11,9 @@ module Subroutine
   end
 
   def self.preserve_time_precision?
-    return false unless defined?(@preserve_time_precision)
+    return !!@preserve_time_precision if defined?(@preserve_time_precision)
 
-    !!@preserve_time_precision
+    false
   end
 
 end
